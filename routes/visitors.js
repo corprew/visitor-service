2f50fddfd2ca80ab856475f46c36e3263998ba68
Corprew Reed <github@corprew.org>
2019-07-09 09:25:35 -0700

    the visitors route ended up not being used

--- routes/visitors.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.post('/visit', function (req, res) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
